58b394130f5f803fc450a0a40d13ce819cde3142
Troe <20bekarys04@gmail.com>
2025-08-07 11:10:03 +0500

    firts commit

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'SaveKidsDB',
-  password: '123456',
-  port: 5432,
+  connectionString: 'postgresql://begarys:Mtgj8mhpY6LrjurGwssTho9bRmBZd6ZS@dpg-d2a1j28gjchc73e52qpg-a.oregon-postgres.render.com/savekids_db_m8y4',
+  ssl:{ rejectUnauthorized: false }
+  // user: 'postgres',
+  // host: 'localhost',
+  // database: 'SaveKidsDB',
+  // password: '123456',
+  // port: 5432,
 });
 
 
